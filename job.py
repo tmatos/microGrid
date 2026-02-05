4b8e0e68e48c0235b7cf289a87d68935f017380d
Tiago Matos <tiagoms88@gmail.com>
2026-02-04 21:16:19 -0300

    add AGPL License and update docs

--- job.py
+++ job.py
@@ -1,3 +1,19 @@
+# microGrid - Sistema de computacao em grid
+# Copyright (C) 2026 Tiago Matos
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 Modulo que implementa a classe Job e Parte para o sistema de distribuicao de tarefas.
 """
