56678d1f095f5b7e94eb2e5596e058b3f3543111
Tiago Matos <tiagoms88@gmail.com>
2026-02-07 22:20:59 -0300

    disable 'broad-except' warning in pylint

--- grid.py
+++ grid.py
@@ -19,6 +19,9 @@ Módulo principal do sistema de computacao em grid.
 Contém o ponto de entrada e a lógica principal do programa.
 """
 
+# TODO: better error handling...
+# pylint: disable=broad-except
+
 from subprocess import call
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 import threading
